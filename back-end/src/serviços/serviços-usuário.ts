f2c4daba8398cc162aeef88226d05194740fc5ae
GuilhermeSsampaio <guilhermessampaio20@gmail.com>
2025-03-10 13:55:02 -0400

    tirar logs

--- back-end/src/serviços/serviços-usuário.ts
+++ back-end/src/serviços/serviços-usuário.ts
@@ -121,7 +121,7 @@ export default class ServiçosUsuário {
       const { cpf, nome, perfil, email, senha, questão, resposta, cor_tema } =
         usuário_informado;
       console.log("ServiçosUsuário.cadastrarUsuário:nome -- " + nome);
-      console.log(JSON.parse(JSON.stringify(usuário_informado)));
+      // console.log(JSON.parse(JSON.stringify(usuário_informado)));
       const cpf_encriptado = md5(cpf);
       const senha_encriptada = await bcrypt.hash(senha, SALT);
       const resposta_encriptada = await bcrypt.hash(resposta, SALT);
