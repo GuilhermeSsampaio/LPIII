aa4dbd25b79a05491538144ae91203b7f03f2538
GuilhermeSsampaio <guilhermessampaio20@gmail.com>
2025-03-21 20:56:56 -0400

    antes de começar a 2 entrega

--- front-end/src/rotas/rotas-aplicação.js
+++ front-end/src/rotas/rotas-aplicação.js
@@ -4,6 +4,9 @@ import LogarUsuário from "../páginas/usuário/logar-usuário";
 import CadastrarUsuário from "../páginas/usuário/cadastrar-usuário";
 import PáginaInicial from "../páginas/usuário/página-inicial";
 import CadastrarMaestro from "../páginas/maestro/cadastrar-maestro";
+import RecuperarAcesso from "../páginas/usuário/recuperar-acesso";
+import CadastrarPatrocinador from "../páginas/patrocinador/cadastrar-patrocinador";
+
 export default function Rotas() {
   return (
     <BrowserRouter>
@@ -14,6 +17,12 @@ export default function Rotas() {
           <Route element={<PáginaInicial />} path="pagina-inicial" />
           <Route element={<CadastrarUsuário />} path="atualizar-usuario" />
           <Route element={<CadastrarMaestro />} path="cadastrar-maestro" />
+
+          <Route element={<RecuperarAcesso />} path="recuperar-acesso" />
+          <Route
+            element={<CadastrarPatrocinador />}
+            path="cadastrar-patrocinador"
+          />
         </Route>
       </Routes>
     </BrowserRouter>
